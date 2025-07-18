628f9f7f54e1b29489b29903b2bf357a76a8245b
MagMc007 <mergashasho7@gmail.com>
2025-07-18 13:07:33 +0300

    include class and function based views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.views.genric import DetailView
-from .models import Books
+from .models import Book
 
 
 def listBooks(request):
     """Obtains all books from databse and renders some html"""
-    books = Books.object.all()
+    books = Book.object.all()
 
     context = {"book_list": books}  #store it in context  as dynamic data
 
-    return render(request, "relationhip_app/templates/list_books.html", context)
+    return render(request, "list_books.html", context)
 
 
 # Create your views here.
 class DisplayDetails(DetailView):
+    model = Book
+    template_name = "library_detail.html"
+    context_object_name = 'library'
     
 
