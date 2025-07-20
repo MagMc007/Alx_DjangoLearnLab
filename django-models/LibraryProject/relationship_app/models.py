381f83924472e4586e3e44432f6f6e0835e40359
MagMc007 <mergashasho7@gmail.com>
2025-07-20 09:11:45 +0300

    implemented role base access control

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserProfile(models.Modle):
+class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'), ('Librarian', 'Librarian'), ('Member', 'Member'),
     ]
