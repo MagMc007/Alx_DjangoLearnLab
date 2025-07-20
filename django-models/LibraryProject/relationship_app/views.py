0473b31b83858f00c56266be8450527f1c75bec9
MagMc007 <mergashasho7@gmail.com>
2025-07-20 10:27:20 +0300

    updated views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -48,16 +48,16 @@ def is_admin(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 @login_required
-@user_passes_test(is_admin, login_url='/relationship_app/login/')
+@user_passes_test(is_admin, login_url='/relationship/login/')
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
 @login_required
-@user_passes_test(is_librarian, login_url='/relationship_app/login/')
+@user_passes_test(is_librarian, login_url='/relationship/login/')
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 @login_required
-@user_passes_test(is_member, login_url='/relationship_app/login/')
+@user_passes_test(is_member, login_url='/relationship/login/')
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
