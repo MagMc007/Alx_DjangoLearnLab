815eb078df9afef11dff6997efbe3b753ab272ce
MagMc007 <mergashasho7@gmail.com>
2025-07-20 09:08:59 +0300

    implemented role base access control

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-# Create your models here.
+class userProfile(models.Modle):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'), ('Librarian', 'Librarian'), ('Member', 'Member'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user} with {self.role}"
+
+        
 class Author(models.Model):
     name = models.CharField(max_length=20)
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 
+from django.contrib.auth.decorators import login_required, user_passes_test
+
 def list_books(request):
     """Obtains all books from databse and renders some html"""
     books = Book.objects.all()
@@ -35,3 +37,26 @@ def register(request):
 
     return render(request, "relationship_app/register.html", {"form": form})
 
+#decorator to check role
+def check_role(role):
+    def role_checker(user):
+        return hasattr(user, 'userprofile') and user.userprofile.role == role
+    return role_checker
+
+
+@login_required
+@user_passes_test(check_role('Admin'))
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+
+@login_required
+@user_passes_test(check_role('Librarian'))
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+
+@login_required
+@user_passes_test(check_role('Member'))
+def member_view(request):
+    return render(request, 'member_view.html')
