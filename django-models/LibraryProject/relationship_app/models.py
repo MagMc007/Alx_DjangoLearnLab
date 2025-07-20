a63496d0662401b57efc03e6a674d10d6fbe6abc
MagMc007 <mergashasho7@gmail.com>
2025-07-20 09:09:41 +0300

    implemented role base access control

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class userProfile(models.Modle):
+class UserProfile(models.Modle):
     ROLE_CHOICES = [
         ('Admin', 'Admin'), ('Librarian', 'Librarian'), ('Member', 'Member'),
     ]
@@ -13,7 +13,7 @@ class userProfile(models.Modle):
     def __str__(self):
         return f"{self.user} with {self.role}"
 
-        
+
 class Author(models.Model):
     name = models.CharField(max_length=20)
 
