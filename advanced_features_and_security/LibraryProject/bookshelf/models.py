650809acc182e637602f63c58d9a93c51b7beea1
MagMc007 <mergashasho7@gmail.com>
2025-07-24 11:33:33 +0300

    added advanced Custom User implementation

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
