f1ac5b68ec8ae8973d048ee3cc443ab6601574b4
MagMc007 <mergashasho7@gmail.com>
2025-07-25 11:23:16 +0300

    created views for users'

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
 
-# Create your views here.
+@permission_required("bookshelf.can_create", raise_exception=True)
+def create_view(request):
+    return HttpResponse("This is the create page")
+
+@permission_required("bookshelf.can_edit", raise_exception=True)
+def view_view(request):
+    return HttpResponse("This is the view page")
+
+@permission_required("bookshelf.can_view", raise_exception=True)
+def edit_view(request):
+    return HttpResponse("This is the edit page")
+
+@permission_required("bookshelf.can_delete", raise_exception=True)
+def delete_view(request):
+    return HttpResponse("This is the delete page")
+
+
+
