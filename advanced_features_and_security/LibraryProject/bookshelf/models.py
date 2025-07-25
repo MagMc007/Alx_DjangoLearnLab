a0b477d0f1c2b8d5e74f9b63c3d25968ff465eec
MagMc007 <mergashasho7@gmail.com>
2025-07-25 11:11:44 +0300

    migrated changes

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -2,14 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-# Create your models here.
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
     def __str__(self):
-        return f"Title: {self.title} by {self.author} publsihed in {self.publication_year}"
+        return (
+            f"Title: {self.title} by {self.author} publsihed in {self.publication_year}"
+        )
 
 
 class CustomUserManager(BaseUserManager):
@@ -17,30 +19,44 @@ class CustomUserManager(BaseUserManager):
         if not username:
             raise ValueError("User needs a username")
         email = self.normalize_email(email)
-        
+
         user = self.model(username=username, email=email)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, username, email, full_name, password=None,**extrafields):
-        extrafields.setdefault('is_staff', True)
-        extrafields.setdefault('is_superuser', True)
+    def create_superuser(
+        self, username, email, full_name, password=None, **extrafields
+    ):
+        extrafields.setdefault("is_staff", True)
+        extrafields.setdefault("is_superuser", True)
 
         return self.create_user(username, email, password, **extrafields)
 
 
-        
-
 """Custom user class for creating custom user"""
+
+
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+    profile_photo = models.ImageField(
+        upload_to="profile_photos/", null=True, blank=True
+    )
 
     objects = CustomUserManager
+
     def __str__(self):
         return self.username
 
 
+"""Custom Permission for users"""
 
-    
+
+class CustomPermission(models.Model):
+    class Meta:
+        permissions = [
+            ("can_view", "Can View"),
+            ("can_create", "Can create"),
+            ("can_edit", "Can Edit"),
+            ("can_delete", "Can delete"),
+        ]
