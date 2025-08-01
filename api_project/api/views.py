a97b5592559e56f85a6422d2b01c462a611a4498
MagMc007 <mergashasho7@gmail.com>
2025-08-01 13:02:59 +0300

    implemented token based auth

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
 from .serializers import BookSerializer
 from rest_framework import generics, viewsets
 from .models import Book
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
