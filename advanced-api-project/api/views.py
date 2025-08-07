7b500b6918dfae1ced39f0edae15c5f0e3048b8b
MagMc007 <mergashasho7@gmail.com>
2025-08-07 21:53:37 +0300

    implemented custom view classes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import (
+    CreateModelMixin, ListModelMixin, RetrieveModelMixin,
+    DestroyModelMixin, UpdateModelMixin
+)
 
-# Create your views here.
+""" this will list existing books """
+class ListView(GenericAPIView, ListModelMixin):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+""" this will retrieve a singe books by id """
+class DetailView(GenericAPIView, RetrieveModelMixin):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+""" this will allow creating of a book """
+class CreateView(GenericAPIView, CreateModelMixin):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+""" this update an existing book """
+class UpdateView(GenericAPIView, UpdateModelMixin):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+
+""" this will delete an existing book """
+class DeleteView(GenericAPIView, DestroyModelMixin):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
