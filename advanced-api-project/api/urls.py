631c7c417387f6269acd1740924519a853ea9cb6
MagMc007 <mergashasho7@gmail.com>
2025-08-07 22:18:24 +0300

    all routes tested and functioning

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,7 @@ from .views import ListView, CreateView, DeleteView, DetailView, UpdateView
 urlpatterns = [
     path("books/", ListView.as_view(), name="list-view"),
     path("books/create/", CreateView.as_view(), name="create-view"),
-    path("books/<int:pk>", DetailView.as_view(), name="detail-view"),
-    path("books/delete/<int:pk>", DeleteView.as_view(), name="delete-view"),
-    path("books/update/<int:pk>", UpdateView.as_view(), name="update-view"),
+    path("books/<int:pk>/", DetailView.as_view(), name="detail-view"),
+    path("books/delete/<int:pk>/", DeleteView.as_view(), name="delete-view"),
+    path("books/update/<int:pk>/", UpdateView.as_view(), name="update-view"),
 ]
