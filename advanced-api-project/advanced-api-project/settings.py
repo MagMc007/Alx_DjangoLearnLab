f187bf5ed7579c35741d572605a485d6102f56db
MagMc007 <mergashasho7@gmail.com>
2025-08-08 15:10:53 +0300

    set up settings for DjangoFilterBackend

--- advanced-api-project/advanced-api-project/settings.py
+++ advanced-api-project/advanced-api-project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "django-filter"
     "api",
     "rest_framework",
     "django.contrib.admin",
@@ -122,3 +123,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ]
+}
