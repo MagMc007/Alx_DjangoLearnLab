9f1129b33445606c2552bc2fb2fae27b3ffcec4e
MagMc007 <mergashasho7@gmail.com>
2025-08-13 16:40:56 +0300

    auth contents complete

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Registration
-
+from .views import Registration, LoginUser, profile, LogoutUser
 
 urlpatterns = [
     path("register/", Registration.as_view(), name="register"),
+    path("login/", LoginUser.as_view(), name="login"),
+    path("profile/", profile, name="home"),
+    path("logout/", LogoutUser.as_view(), name="logout")
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,10 +1,28 @@
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-#from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 class Registration(CreateView):
-    form_class = UserCreationForm
-    # success_url = reverse_lazy()
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
     template_name = "blog/register.html"
+
+
+class LoginUser(LoginView):
+    template_name = "blog/login.html"
+    next_page = reverse_lazy("home")
+
+
+@login_required
+def profile(request):
+    return render(request, "blog/profile.html")
+
+
+class LogoutUser(LogoutView):
+    template_name = "blog/logout.html"
+    next_page = reverse_lazy("login")
