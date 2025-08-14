70c04971a541872ebb10f561d73a5670be7e056c
MagMc007 <mergashasho7@gmail.com>
2025-08-14 16:18:53 +0300

    all functionalities tested and fixed

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,16 +1,14 @@
 from django.contrib.auth.forms import UserChangeForm
 from django.views.generic import CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from .forms import CustomUserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from rest_framework import generics
-from .serializer import PostSerializer
 from .models import Post
 from django.views.generic import (
-    ListView,CreateView, DeleteView, DetailView, UpdateView
+    ListView, CreateView, DeleteView, DetailView, UpdateView
 )
 from .forms import PostForm
 
@@ -50,14 +48,14 @@ def edit_profiles(request):
 
 
 class PostListView(ListView):
-    """ for listing all post contents """
+    """ list all posts """
     model = Post
     template_name = "blog/post_list.html"
     context_object_name = "posts"
 
 
 class PostDetailView(DetailView):
-    """ for viewing post """
+    """ this one is for individual post access"""
     model = Post
     template_name = "blog/post_detail.html"
     context_object_name = "post"
@@ -67,7 +65,8 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     """ for creating a post """
     model = Post
     form_class = PostForm
-    template_name = "blog/post_form.html"
+    template_name = "blog/post_create.html"
+    success_url = reverse_lazy("list-view")
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -75,16 +74,22 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    """ to update a post """
+    """ to update existing post"""
     model = Post
     form_class = PostForm
-    template_name = "blog/post_form.html"
+    # same template used for creating
+    template_name = "blog/post_create.html"
 
     def test_func(self):
         return self.get_object().author == self.request.user
+    
+    # for redirect upon completion of update
+    def get_success_url(self):
+        return reverse("detail-view", kwargs={"pk": self.object.pk})
 
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """ for deleting a post """
     model = Post
     template_name = "blog/post_confirm_delete.html"
     success_url = reverse_lazy("list-view")
