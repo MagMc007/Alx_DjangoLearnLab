5ad6aa37dea1602c8f4d6897c2bc13d3ba674daa
MagMc007 <mergashasho7@gmail.com>
2025-08-14 12:32:01 +0300

    templates made

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("edit_profile/", views.edit_profiles, name="edit_profile"),
     path("logout/", views.LogoutUser.as_view(), name="logout"),
     path("posts/", views.ListView.as_view(), name="list-view"),
+    path("post/new/", views.CreateView.as_view(), name="list-view"),
     path("posts/<int:pk>", views.DetailView.as_view(), nama="detail-view"),
     path("posts/<int:pk>/edit", views.UpdateView.as_view(), name="update-view"),
     path("posts/<int:pk>/delete", views.DeleteView.as_view(), name="delete-view"),
