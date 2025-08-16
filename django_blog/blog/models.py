ef1c98d71bf26c9415f572c8a4728ffb4e394081
MagMc007 <mergashasho7@gmail.com>
2025-08-16 09:07:29 +0300

    migrated the tag model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -29,3 +29,10 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    post = models.ManyToManyField(Post, related_name="tagged")
+
+    
