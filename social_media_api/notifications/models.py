7d05bc718e23b9a2cab8af082ad76d5ee420da31
MagMc007 <mergashasho7@gmail.com>
2025-08-20 22:07:47 +0300

    migrated all changes in notifications app

---
+++ social_media_api/notifications/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+User = get_user_model()
+
+
+class Notification(models.Model):
+    """ a model for notifications between users """
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
+    actor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="actions")
+    verb = models.CharField(max_length=255)
+
+    # Generic relation on comment, post or some other model with in the projects
+    target_content = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name="targeted_content")
+    target_object_id = models.PositiveIntegerField()
+    target = GenericForeignKey("target_content", "target_object_id")
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "notifications",
     "django_filters",
     "posts",
     "rest_framework.authtoken",
